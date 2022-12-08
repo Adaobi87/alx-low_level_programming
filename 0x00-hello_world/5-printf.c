ce57415df7bec9f4ec77e8e72eb98158da22ad2c
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-08 12:03:04 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,11 @@
 
 /**
  * main - Entry point
- * 
- * description - print with proper grammar, but the outcome is a piece of art
  *
- * return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-
-int main(void)
+int main void
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+} 
