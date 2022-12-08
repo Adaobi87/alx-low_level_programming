f2a15f000ba0b33d23197b4ba11caafb00e9edf2
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-08 12:34:08 -0800

    size is not grandier

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Entry point
- * 
+ *
  *Description:Prints the various types of data
  *
  *  Return:0
  */
 
 int main(void)
-{ 
+{
 	printf("size of a char:%zu byte(s)\n", sizeof(char));
 	printf("size of an int:%zu byte(s)\n", sizeof(int));
 	printf("size of a long int:%zu byte(s)\n", sizeof(long int));
