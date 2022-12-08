b63e36df227986b4cb57e07b0235b993b01d2c4b
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-08 14:57:12 -0800

    size is not grandier

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,7 @@
 
 /**
  * main - Entry point
+ *
  * return: Always 0 (Success)
  */
 int main(void)
@@ -11,10 +12,10 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
-	printf("Size of a char: %lu byte(s)\n",sizeof(d));
-	printf("Size of an int: %lu byte(s)\n",sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n",sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n",sizeof(c));
-	printf("Size of a float: %lu byte(s)\n",sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
