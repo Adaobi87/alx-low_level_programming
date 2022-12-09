217eacbbacc10148404a3e28de009a2e102cf393
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-09 12:20:48 -0800

    Single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints single digits of base 10
+ *
+ * return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	for(i = 0; i < 10; i++)
+		printf("%d\n", i);
+	return (0);
+}
