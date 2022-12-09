e18a673364524a737bad3c56cbfc5151c8de6ffd
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-09 13:00:21 -0800

    single digitsof base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
