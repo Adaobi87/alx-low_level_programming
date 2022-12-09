877933c39568916db6428678290eeb02722549f1
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-09 13:19:17 -0800

    lower case of alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description- prints single digits of
+ * base 10
+ *
+ * return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
