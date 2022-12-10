0c22368722cb5c8fb4f0b317f30f06991d796771
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-09 18:42:54 -0800

    print combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 
 /**
- * main - prints combination of single digit.
+ * main - Entry point
  *
- * return: returns 0 always (success)
+ * return: Always 0 (success)
  */
-
 int main(void)
 {
 	int d;
 
 	for (d = '0'; d <= '9'; d++)
-		putchar(d);
 	{
-		if ( d != 9 );
-		putchar(',');
-		putchar(' ');
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
 }
+
 
 	
