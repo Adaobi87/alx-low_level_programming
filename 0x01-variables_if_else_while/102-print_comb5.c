548c61c743e2657dd2df2bd72c4cd7ec6840ad85
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-09 20:27:59 -0800

    print combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (p = 0; p <= 98; p++)
 	{
-		for (q = p + 1; q <= 98; q++)
+		for (q = p + 1; q <= 99; q++)
 		{
 			putchar((p / 10) + '0');
 			putchar((p % 10) + '0');
@@ -21,7 +21,7 @@ int main(void)
 			putchar((q / 10) + '0');
 			putchar((q % 10) + '0');
 
-			if(p == 98 && q == 98)
+			if (p == 98 && q == 99)
 				continue;
 
 			putchar(',');
