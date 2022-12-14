8db7bad20fce9cfc849ad0d0864fdf18c6b2050b
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-13 18:25:40 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,15 +9,41 @@
   */
 void print_times_table(int n)
 {
-	int i, j;
-
-	if (n > 0 && n < 15)
+	int i, j, res;
+	
+	if(!(n > 15 || n < 0))
 	{
-		for (i = 0; i <= n; i++)
+		for(i = 0; i <= n; i++)
 		{
-			_putchar('0');
-			for (j = 1; j <= n; j++)
-				_putchar(i * j);
+			for(j = 0; j <= n; j++)
+			{
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(' , ');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if ( res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
 			_putchar('\n');
 		}
 	}
