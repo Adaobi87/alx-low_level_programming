f47a161423684637c736f8939b002e3a14743b8e
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-13 19:12:30 -0800

    print to 8

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,23 +5,21 @@
   * print_to_98 - print numbers from input to 98
   *
   * @n: A input integer
+  *
+  * Return: return nothing
   */
 void print_to_98(int n)
 {
-	if (n <= 98);
+	if (n < 98);
 	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-				continue;
-			printf(" , ");
-		printf("\n");
-		for (; n >= 98; n--)
-			printf("%d", n);
-			if (n == 98)
-				continue;
-			printf(", ");
-		}
-		printf("\n");
+		for (n = n; n <= 98; n++)
+		
+			printf("%d, ", n);
+		printf("%d\n", 98);
+		
+		for (n = n; n > 98; n--)
+		
+			printf("%d, ", n);
+		printf("%d\n", 98);
 	}
 }
