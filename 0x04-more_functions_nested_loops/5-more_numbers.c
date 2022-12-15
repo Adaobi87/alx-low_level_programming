faa0abda3f333a544bed44a9db3e42530ebdc068
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-15 04:36:41 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,12 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 14; i++)
 	{
-		for (j = 0; j <= 15; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			_putchar(j + '0');
+			_putchar(i + '0');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
