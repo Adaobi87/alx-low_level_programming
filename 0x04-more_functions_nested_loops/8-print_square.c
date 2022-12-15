f886d3fe3f2341cc14715b497e856efb50213757
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-15 06:04:18 -0800

    print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_square - function that print a square
+  *
+  * @size: integer parameter
+  */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
