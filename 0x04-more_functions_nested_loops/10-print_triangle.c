a145e794fedfa18f37992f897d7a5e2adff80954
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-15 20:56:37 -0800

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,9 +12,9 @@ void print_triangle(int size)
 	if (size <= 0)
 		_putchar('\n');
 	else
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = i; j < size; j++)
 		{
 			_putchar(' ');
 		}
