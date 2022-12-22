d44ecb39af170d24bafb6ba184a65f248260a193
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-22 01:48:26 -0800

    rot-13

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * leet - function that encodes a string into 1337.
+  * Letters a and A should be replaced by 4
+  * Letters e and E should be replaced by 3
+  * Letters o and O should be replaced by 0
+  * Letters t and T should be replaced by 7
+  * Letters l and L should be replaced by 1
+  */
+char *leet(char *)
+{
+	int stringCount, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+
+	stringCount = 0;
+	while (s[stringCount] != '\0')
+	{
+		leetCount = 0;
+		while (leetCount < 10)
+		{
+			if (leetLetters[leetCount]== s[stringCount])
+			{
+				s[stringCount] = leetNums[leetCount];
+			}
+			stringCount++;
+		}
+		return (s);
+	}
+
