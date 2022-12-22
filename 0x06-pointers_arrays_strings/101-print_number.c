995297f2f4e77c3c39f416d39b5b2d6a5424c2ad
Adaobi Okoye <adaobiokoye87@gmail.com>
2022-12-22 03:05:18 -0800

    print integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@ void print_number(int n)
 {
 	unsigned int num;
 
-	num = -n;
+	num = n;
 	if (n < 0)
 	{
 		_putchar(45);
@@ -16,7 +16,7 @@ void print_number(int n)
 	}
 	if (num / 10)
 	{
-		print_number(num / 10);
+		_putchar(num / 10);
 	}
 	_putchar((num % 10) + '0');
 }
