c9433123440b8601ea25542547e2d5b139efec1b
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-01-26 05:01:06 -0800

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	for (n = 0; n < str[n]; n++)
+	for (n = 0; str[n]; n++)
 		;
 	new_node->len = n;
 	new_node->next = *head;
