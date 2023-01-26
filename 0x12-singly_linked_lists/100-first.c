bd33d5e80494049ba1c5af85dd9f599d95b1f1ec
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-01-26 05:25:37 -0800

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * first - function executed before main
  * Return: no return.
  */
-void first(void)
+void __attribute__ ((constructor)) first()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
