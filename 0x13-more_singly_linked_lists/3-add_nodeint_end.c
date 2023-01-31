6f58e7a36d78057df58c6648a26d81c2242c38ab
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-01-30 22:03:06 -0800

    add node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,7 +21,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	if (cursor != NULL)
 	{
-		while (cursor != NULL)
+		while (cursor->next != NULL)
 			cursor = cursor->next;
 		cursor->next = new_node;
 	}
