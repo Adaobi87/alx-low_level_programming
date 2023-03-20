d08869c77cd35ae460cb40b88dfa28f547a6e1d9
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-03-20 07:14:40 -0700

    libdynamic

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _strncat - function that concatenate two strings
+  * @dest: first parameter
+  * @src: 2nd parameter
+  * @n: number of bytes to be concatenated
+  * Return: pointer to destination string
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, j;
+
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+		dest[length] = '\0';
+		return (dest);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+The functions have already created earlier but this task, I make use of the folder funcs which contains the coded functions
